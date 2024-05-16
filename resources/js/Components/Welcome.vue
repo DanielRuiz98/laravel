833a47ddc3349000c81097633b68087eb82cbd40
DanielRuiz98 <danruimir@alu.edu.gva.es>
2024-05-16 15:41:34 +0200

    Commit inicial

---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { usePage } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div>
+        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+        
+            <img src="../../../public/storage/profile-photos/D2XihmqV4asr8i8B2x3kJNrAIptcFxXj60vNOeAg1.png">
+            
+           
+        </div>
+    </div>
+</template>
---
+++ resources/views/emails/testemail.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>
+            HOLA
+        </title>
+    </head>
+    <body>
+    EEYYYY
+    </body>
+</html>
